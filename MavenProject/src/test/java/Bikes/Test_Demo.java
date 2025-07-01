572d53350a412a053afe2cbe14fe2cf2fbfb3731
Sunil Samay <Sunil Samay@MSI>
2025-07-01 13:19:11 +0530

    Added launch

---
+++ MavenProject/src/test/java/Bikes/Test_Demo.java
@@ -0,0 +1,14 @@
+package Bikes;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test_Demo {
+	@Test
+	public void launch()
+	{
+	   WebDriver driver = new ChromeDriver();
+	}
+
+}
